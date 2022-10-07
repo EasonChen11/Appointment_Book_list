95fdbd24780156dcb0b2bc23f7accb26d2542f72
Your Name <you@example.com>
2022-10-07 23:08:40 +0800

    Lab02a create a chice

--- Lab02a.c
+++ Lab02a.c
@@ -1,5 +1,6 @@
 //
 // Created by Eason chen on 2022/10/6.
+//finish by Eason chen on 2022/10/7.
 //
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,6 +9,10 @@
 #include <string.h>
 #define HOWMANY 100//how much of random number
 #define filename_size 20
+
+enum Choice{
+    Random=1,Readfile
+};
 struct sc{
     int score;
     struct sc *next;
@@ -24,17 +29,42 @@ void printScore(scType *head);
 
 void output_file(scType * head,char *filename);
 
+int menu();
+
+void read_filename(char * filename);
+
 int main(){
     char input_filename[filename_size],output_filename[filename_size];
     scType *A=NULL;
-    random_data(input_filename);
+    switch (menu()) {
+        case Random:
+            random_data(input_filename);
+            break;
+        case Readfile:
+            read_filename(input_filename);
+            break;
+    }
     readfile_and_sorting(&A,input_filename);
     output_file(A,output_filename);
 }
 
+void read_filename(char * filename) {
+    printf("Please enter the file name to open:(example:./data.txt)");
+    scanf("%s",filename);//read output filename
+    rewind(stdin);//clear
+    assert(strlen(filename)<filename_size);//check the filename size legitimate
+}
+
+int menu() {
+    printf("1.random a data and sorting. 2.read a file:");
+    int choice;
+    scanf("%d",&choice);
+    rewind(stdin);
+    return choice;
+}
+
 void output_file(scType * head,char *filename) {//put the data to txt
     printf("Please enter the file name to open:(example:./output.txt)");
-    //fgets(filename, filename_size,stdin);
     scanf("%s",filename);//read output filename
     rewind(stdin);//clear
     assert(strlen(filename)<filename_size);//check the filename size legitimate
@@ -57,7 +87,7 @@ void output_file(scType * head,char *filename) {//put the data to txt
 }
 
 void random_data(char *filename) {//create random numbers file
-    printf("Please enter the file name to open:(example:./data.txt)");
+    printf("Please enter the file name to create:(example:./data.txt)");
     scanf("%s",filename);//input filename
     rewind(stdin);
     assert(strlen(filename)<filename_size);//check the filename size legitimate
@@ -68,10 +98,8 @@ void random_data(char *filename) {//create random numbers file
     }
     srand(time(0));
     int i=0;
-    printf("random data:\n");
     while (i++ <HOWMANY){
         int k=rand()%HOWMANY;
-        printf("%d ",k);
         fprintf(fp,"%d ",k);
     }//run HOWMANY times
     printf("\n");
@@ -83,15 +111,18 @@ void readfile_and_sorting(scType ** head,char *filename) {//read numbers
     FILE *fp;
     if( (fp = fopen(filename,"r")) == NULL){//check open file legitimate
         fclose(fp);
+        printf("%s is not exist\n",filename);
+        exit(1);//exit code whether the file can't read
         return;
     }
-    printf("read file:%s\n",filename);
+    printf("read file:%s\ndata:\n",filename);
     int score;
     while ( fscanf(fp, "%d",&score)!=EOF){
+        printf("%d ",score);
         addScore_and_sorting(head,score);//add number in the link list
     }
     fclose(fp);
-    printf("sorting:\n");
+    printf("\nsorting:\n");
     printScore(*head);
 }
 
@@ -99,7 +130,6 @@ void addScore_and_sorting(scType ** head, int score) {
     scType *save= (scType*)malloc(sizeof (scType));
     save->score=score;
     save->next=NULL;//create a node
-    //printf("%d\n",save->score);
     if(*head==NULL){//first is NULL
         *head=save;
         return;
@@ -121,7 +151,7 @@ void addScore_and_sorting(scType ** head, int score) {
     }
     before->next=save;
 }
-void printScore(scType *head){
+void printScore(scType *head){//print data
     while (head){
         printf("%d ",head->score);
         head=head->next;
